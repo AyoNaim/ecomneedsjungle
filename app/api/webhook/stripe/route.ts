17b46c7f8a01c0a62da7cb8ec217fc2dc12e7b03
AyoNaim <121489914+AyoNaim@users.noreply.github.com>
2026-05-13 18:59:53 +0000

    setup stripe webhook

---
+++ app/api/webhook/stripe/route.ts
@@ -0,0 +1,36 @@
+import { stripe } from "@/lib/stripe";
+import { prisma } from "@/lib/prisma";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+    const body = await req.text()
+    const signature = (await headers()).get("Stripe-Signature") as string;
+
+    let event;
+    try {
+        event = stripe.webhooks.constructEvent(
+                body,
+                signature,
+                process.env.STRIPE_WEBHOOK_SECRET!
+            )
+    } catch (error: any) {
+        NextResponse.json(`Webhook Error: ${error.message}`, { status: 400 })
+    }
+
+    const session = event?.data.object as any;
+if (event?.type === "checkout.session.completed") {
+    // Sync with your database here
+    await prisma.order.create({
+      data: {
+        userId: session.metadata.userId,
+        productId: session.metadata.productId,
+        amount: session.amount_total / 100,
+        status: "COMPLETED",
+        paymentMethod: "STRIPE",
+      },
+    });
+    
+  }
+  return new NextResponse(null, { status: 200 });
+}
