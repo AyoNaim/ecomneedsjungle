9195a4058d66e4a110d97d1ac96efd16576077f6
AyoNaim <121489914+AyoNaim@users.noreply.github.com>
2026-07-12 10:05:29 +0000

    back to data.widget url

--- app/api/checkout/transak/route.ts
+++ app/api/checkout/transak/route.ts
@@ -74,8 +74,8 @@ export async function POST(request: Request) {
 
     const data = await response.json();
     console.log("data returned by transak", data)
-    return NextResponse.json({ data_retured: data });
-    // return NextResponse.json({ widgetUrl: data.data?.widgetUrl || data.widgetUrl });
+    // return NextResponse.json({ data_retured: data });
+    return NextResponse.json({ widgetUrl: data.data?.widgetUrl || data.widgetUrl });
   } catch (error: any) {
     console.error("Transak session failure:", error.message);
     return NextResponse.json({ error: error.message || 'Failed to initialize secure session' }, { status: 500 });
