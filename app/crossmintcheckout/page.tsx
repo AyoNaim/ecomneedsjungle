8540189de8add024d903750ff7f46d75ccf1d6f1
AyoNaim <121489914+AyoNaim@users.noreply.github.com>
2026-07-14 03:45:25 +0000

    completed changeNOW

--- app/crossmintcheckout/page.tsx
+++ app/crossmintcheckout/page.tsx
@@ -1,10 +1,11 @@
+import ChangeNowCheckoutTerminal from "@/components/ChangeNOW";
 import SendtoCheckout from "@/components/SendtoCheckout";
 import TransakCheckoutTerminal from "@/components/TransakWidgetModal";
 
 export default function Page() {
   return (
     <div>
-      <TransakCheckoutTerminal />
+      <ChangeNowCheckoutTerminal />
     </div>
   );
 }
